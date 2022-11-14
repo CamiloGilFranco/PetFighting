8f1613507bedde2f7e72d9c8d9ea48885a5e6e0e
Camilo Gil Franco <camilogilfranco@gmail.com>
2022-11-14 00:00:44 -0500

    Funcionalidades con DOM

--- scripts/PetFighting.js
+++ scripts/PetFighting.js
@@ -1,12 +1,38 @@
+let ataqueJugador;
+let ataqueEnemigo;
+let vidasEnemigo = 3;
+let vidasJugador = 3;
+
 function iniciarJuego() {
+  let secciónAtaques = document.getElementById("seleccionar-ataque");
+  secciónAtaques.style.display = "none";
+
   let botonSeleccionarMascota = document.getElementById(
     "boton-seleccionar-mascota"
   );
 
   botonSeleccionarMascota.addEventListener("click", seleccionarMascota);
+
+  let botonAgua = document.getElementById("boton-agua");
+  botonAgua.addEventListener("click", ataqueAgua);
+
+  let botonFuego = document.getElementById("boton-fuego");
+  botonFuego.addEventListener("click", ataqueFuego);
+
+  let botonTierra = document.getElementById("boton-tierra");
+  botonTierra.addEventListener("click", ataqueTierra);
+
+  let botonReiniciar = document.getElementById("boton-reiniciar");
+  botonReiniciar.addEventListener("click", reiniciarJuego);
 }
 
 function seleccionarMascota() {
+  let secciónAtaques = document.getElementById("seleccionar-ataque");
+  secciónAtaques.style.display = "block";
+
+  let secciónMascotas = document.getElementById("seleccionar-mascota");
+  secciónMascotas.style.display = "none";
+
   let inputDraco = document.getElementById("Draco");
   let inputMolly = document.getElementById("Molly");
   let inputViole = document.getElementById("Viole");
@@ -14,9 +40,6 @@ function seleccionarMascota() {
   let inputTron = document.getElementById("Tron");
   let inputMota = document.getElementById("Mota");
   let nombreMascotaJugador = document.getElementById("nombre_mascota_jugador");
-  let nombreMascotaEnemigo = document.getElementById("nombre_mascota_enemigo");
-  let vidasJugador = document.getElementById("vidas_jugador");
-  let vidasEnemigo = document.getElementById("vidas_enemigo");
 
   if (inputDraco.checked) {
     nombreMascotaJugador.innerHTML = "Draco";
@@ -33,6 +56,123 @@ function seleccionarMascota() {
   } else {
     alert("No haz seleccionado una mascota");
   }
+  seleccionarMascotaEnemigo();
+}
+
+function seleccionarMascotaEnemigo() {
+  let mascotaAleatoria = aleatorio(1, 6);
+  let nombreMascotaEnemigo = document.getElementById("nombre_mascota_enemigo");
+
+  if (mascotaAleatoria === 1) {
+    nombreMascotaEnemigo.innerHTML = "Draco";
+  } else if (mascotaAleatoria === 2) {
+    nombreMascotaEnemigo.innerHTML = "Molly";
+  } else if (mascotaAleatoria === 3) {
+    nombreMascotaEnemigo.innerHTML = "Viole";
+  } else if (mascotaAleatoria === 4) {
+    nombreMascotaEnemigo.innerHTML = "Rex";
+  } else if (mascotaAleatoria === 5) {
+    nombreMascotaEnemigo.innerHTML = "Tron";
+  } else if (mascotaAleatoria === 6) {
+    nombreMascotaEnemigo.innerHTML = "Mota";
+  }
+}
+
+function ataqueAgua() {
+  ataqueJugador = "Agua";
+  ataqueAleatorioEnemigo();
+}
+
+function ataqueFuego() {
+  ataqueJugador = "Fuego";
+  ataqueAleatorioEnemigo();
+}
+
+function ataqueTierra() {
+  ataqueJugador = "Tierra";
+  ataqueAleatorioEnemigo();
+}
+
+function ataqueAleatorioEnemigo() {
+  let ataqueAleatorio = aleatorio(1, 3);
+
+  if (ataqueAleatorio === 1) {
+    ataqueEnemigo = "Agua";
+  } else if (ataqueAleatorio === 2) {
+    ataqueEnemigo = "Fuego";
+  } else if (ataqueAleatorio === 3) {
+    ataqueEnemigo = "Tierra";
+  }
+
+  combate();
+}
+
+function combate() {
+  let spanVidasJugador = document.getElementById("vidas_jugador");
+  let spanVidasEnemigo = document.getElementById("vidas_enemigo");
+
+  if (ataqueJugador === ataqueEnemigo) {
+    crearMensaje("EMPATASTE 😐");
+  } else if (ataqueJugador === "Agua" && ataqueEnemigo === "Fuego") {
+    crearMensaje("GANASTE 😎");
+    vidasEnemigo--;
+    spanVidasEnemigo.innerHTML = vidasEnemigo;
+  } else if (ataqueJugador === "Fuego" && ataqueEnemigo === "Tierra") {
+    crearMensaje("GANASTE 😎");
+    vidasEnemigo--;
+    spanVidasEnemigo.innerHTML = vidasEnemigo;
+  } else if (ataqueJugador === "Tierra" && ataqueEnemigo === "Agua") {
+    crearMensaje("GANASTE 😎");
+    vidasEnemigo--;
+    spanVidasEnemigo.innerHTML = vidasEnemigo;
+  } else {
+    crearMensaje("PERDISTE 😞");
+    vidasJugador--;
+    spanVidasJugador.innerHTML = vidasJugador;
+  }
+
+  revisarVidas();
+}
+
+function revisarVidas() {
+  if (vidasEnemigo === 0) {
+    crearMensajeFinal("🎉🎉 HAZ GANADO EL DUELO 🎉🎉");
+  } else if (vidasJugador === 0) {
+    crearMensajeFinal("😭😭 HAZ PERDIDO EL DUELO 😭😭");
+  }
+}
+
+function crearMensajeFinal(resultadoFinal) {
+  let secciónMensajes = document.getElementById("mensajes");
+
+  let párrafo = document.createElement("p");
+  párrafo.innerHTML = resultadoFinal;
+  secciónMensajes.appendChild(párrafo);
+
+  let botonAgua = document.getElementById("boton-agua");
+  botonAgua.disabled = true;
+
+  let botonFuego = document.getElementById("boton-fuego");
+  botonFuego.disabled = true;
+
+  let botonTierra = document.getElementById("boton-tierra");
+  botonTierra.disabled = true;
+}
+
+function crearMensaje(resultado) {
+  let secciónMensajes = document.getElementById("mensajes");
+
+  let párrafo = document.createElement("p");
+  párrafo.innerHTML = `Haz atacado con ${ataqueJugador}, tu enemigo ataco con ${ataqueEnemigo}, ${resultado}`;
+  secciónMensajes.appendChild(párrafo);
+}
+
+function aleatorio(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function reiniciarJuego() {
+  location.reload();
 }
 
 window.addEventListener("load", iniciarJuego);
